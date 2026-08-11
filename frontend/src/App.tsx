72c19af3aea64f07b24dca2f27b1a88fe464b484
Cleiton <desenvolvimentoagente9@gmail.com>
2026-08-11 18:08:53 -0300

    chore: configuracao do projeto React com Vite, Tailwind, shadcn/ui e tema personalizado

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-cream">
+      <Button className="bg-primary hover:bg-primary-hover text-white">
+        Teste do tema
+      </Button>
+    </div>
+  );
+}
+
+export default App;
